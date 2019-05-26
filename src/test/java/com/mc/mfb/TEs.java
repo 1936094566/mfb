c76320780cda2082b83c2c7cabcc5069c1afa267
machao0605 <machao0605@qq.com>
2019-05-26 15:25:32 +0800

    修改持久层 mybatis-pLUS

---
+++ src/test/java/com/mc/mfb/TEs.java
@@ -0,0 +1,14 @@
+package com.mc.mfb;
+
+/**
+ * @Created with IDEA
+ * @author:麻超
+ * @Date:2019/5/22
+ * @Time:16:04
+ **/
+public class TEs {
+    public static void main(String[] args) {
+        if("sss".equals(null)){
+        }
+    }
+}
