05f096a9cbb7003aa8387a839b7d5b7eef5f6ede
machao0605 <machao0605@qq.com>
2019-11-09 07:49:58 +0800

    提交1109

--- src/main/java/java8/interfacedemo/McBest.java
+++ src/main/java/java8/interfacedemo/McBest.java
@@ -8,12 +8,15 @@ import java.util.Random;
  * @Date:2019/10/12
  * @Time:1:36
  **/
-public class McBast {
+public class McBest {
     public static void main(String[] args) {
         Random random = new Random();
         int x  = random.nextInt(8888);
         for (int i = 0; i<=x ; i++){
-            System.out.println(" 麻超是最棒的!!!!! 不容反驳!!!");
+            System.out.println(" 麻超是最棒的!!!!!! 不容反驳!!!太赞了!!!");
+            System.out.println("很帅的我");
+            System.out.println("最帅最好看的我!");
+            System.out.println("我必定帅气,我必定优秀");
             System.out.println(x);
         }
 
